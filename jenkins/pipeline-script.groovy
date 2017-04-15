3b49a4f7eb7d5dc9c7f0d8d18e94d1e9ffb4a3b2
yurayuramen <yurayuramen@yahoo.co.jp>
2017-04-15 21:56:28 +0900

    jenkins関連

---
+++ jenkins/pipeline-script.groovy
@@ -0,0 +1,28 @@
+node {
+
+   echo 'start'
+   
+    stage('Git') {
+      git 'https://github.com/yurayuramen/try-ci.git'
+    }
+   
+    checkout([$class: 'GitSCM',
+      userRemoteConfigs: [[
+        url: 'https://github.com/yurayuramen/try-ci.git'
+      ]],
+      branches: [[name: "origin/${githubSourceBranch}"]] // ïœêîìWäJÇ≈Ç´ÇÈÇÊÇ§Ç… &quot; Ç≈àÕÇ§Ç±Ç∆
+    ])
+   
+   stage('Build') {
+        def builds = [:]
+        builds['scala'] = {
+           // assumes you have the sbt plugin installed and created an sbt installation named &apos;sbt-0.13.13&apos;
+           // sh "${tool name: 'sbt-0.13.13', type: '/usr/local/sbt/bin/sbt compile dist"
+           sh '/usr/local/sbt/bin/sbt compile dist'
+        }
+        builds['frontend'] = {
+           echo 'building the frontend';
+        }
+       parallel builds
+    }
+}
